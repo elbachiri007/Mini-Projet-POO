4199df0c13d88798ca69c30020626fab9827120a
essarraj.fouad <essarraj.fouad@gmail.com>
2019-12-20 12:33:45 +0100

    Ajoutez des fichiers projet.

---
+++ Creation_Objet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creation_Objet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personne personne;
+            personne = new Personne();
+
+            Personne p2 = new Personne("Madani");
+
+            Console.WriteLine(personne);
+            Console.WriteLine(p2);
+
+            Console.ReadKey();
+        }
+    }
+}
